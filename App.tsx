b74f74dd88fbe25cbe3804e0bc9b8c97e6dd3b7c
Lee Hangsen <leehangsen80@gmail.com>
2024-07-22 00:38:25 +0800

    feature: added redux persist and persist logic

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import Navigation from "./app/src/routes/Navigation";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import appStore from "./app/redux/appStore";
 
 const App = () => {
   return (
-    <Provider store={appStore}>
-      <Navigation />
+    <Provider store={appStore.appStore}>
+      <PersistGate loading={null} persistor={appStore.appPersistor}>
+        <Navigation />
+      </PersistGate>
     </Provider>
   );
 };
--- app/redux/appStore.ts
+++ app/redux/appStore.ts
@@ -1,13 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { loginSlice } from "./appSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const loginPersistConfig = {
+  key: "login",
+  storage: AsyncStorage,
+};
+
+const loginPersistReducer = persistReducer(
+  loginPersistConfig,
+  loginSlice.reducer
+);
 
 const appStore = configureStore({
   reducer: {
-    login: loginSlice.reducer,
+    login: loginPersistReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
-export default appStore;
+const appPersistor = persistStore(appStore);
 
+export default { appStore, appPersistor };
 export type RootState = ReturnType<typeof appStore.getState>;
 export type AppDispatch = typeof appStore.dispatch;
